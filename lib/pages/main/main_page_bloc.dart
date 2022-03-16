0930b5645cc66330c05dd3223c86099444a8f1dd
Jones <jlindb18@student.aau.dk>
2022-03-16 11:04:05 +0100

    upup

--- lib/pages/main/main_page_bloc.dart
+++ lib/pages/main/main_page_bloc.dart
@@ -47,7 +47,7 @@ class MainPageBloc extends Bloc<MainPageEvent, MainPageState> {
       : super(MainPageState(
             hasJustPerformedAction: false,
             isPlaylistShown: false,
-            quackLocationType: QuackLocationType.nightLife,
+            quackLocationType: QuackLocationType.unknown,
             isRecommendationStarted: false)) {
     /// ButtonPressed
     on<ButtonPressed>((event, emit) async {
--- lib/values/values.dart
+++ lib/values/values.dart
@@ -37,12 +37,12 @@ const String appFeatureImage2 = "assets/feature_image2.svg";
 
 /// Quack Location Type Images
 const String qltBeach = "assets/cuack_locationtypes/beach.jpg";
-const String qltEducation = "assets/cuack_locationtypes/education.jpg";
+const String qltEducation = "assets/cuack_locationtypes/altEducation.jpg";
 const String qltCemetery = "assets/cuack_locationtypes/cemetery.jpg";
 const String qltChurch = "assets/cuack_locationtypes/church.jpg";
 const String qltForest = "assets/cuack_locationtypes/forest.jpg";
 const String qltNightLife = "assets/cuack_locationtypes/nightLife.jpg";
-const String qltUnknown = "assets/cuack_locationtypes/unknown.png";
+const String qltUnknown = "assets/cuack_locationtypes/unknown.jpeg";
 const String qltUrban = "assets/cuack_locationtypes/urban.jpg";
 
 
